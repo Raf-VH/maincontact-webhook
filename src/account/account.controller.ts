cc0b985ff8cacd80fe7a54ca9db60af01cf56870
Raf-VH <raf.vanhoegaerden@outlook.com>
2025-02-13 14:05:29 +0100

    Add logic to services

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
+import { AccountService } from './account.service';
+import {
+    Accountfile,
+    Accountqueryresponse
+} from 'services/SalesSvcCloudV2_accountService';
+import { ContactPersonfile } from 'services/SalesSvcCloudV2_contactPersonService';
+import { ContactService } from 'src/contact/contact.service';
 
-@Controller('account')
-export class AccountController {}
+@Controller('accounts')
+export class AccountController {
+    constructor(
+        private readonly accountservice: AccountService,
+        private readonly contactservice: ContactService
+    ) {}
+
+    @Get()
+    getAccounts(): Promise<Accountqueryresponse> {
+        return this.accountservice.getAccounts();
+    }
+
+    @Get(':Id')
+    getAccount(@Param('Id') accountId: string): Promise<Accountfile> {
+        return this.accountservice.getAccountById(accountId);
+    }
+
+    @Get('/:accountId/maincontact')
+    getMainContact(
+        @Param('accountId') accountId: string
+    ): Promise<ContactPersonfile> {
+        return this.contactservice.GetMainContactAccountId(accountId);
+    }
+
+    @Post()
+    postAccount() {}
+}
--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,4 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, Logger } from '@nestjs/common';
+import {
+    AccountApi,
+    Accountfile,
+    Accountqueryresponse
+} from '../../services/SalesSvcCloudV2_accountService';
+import { DestinationService } from 'src/destination/destination.service';
 
 @Injectable()
-export class AccountService {}
+export class AccountService {
+    constructor(private readonly destinationService: DestinationService) {}
+
+    async getAccounts(): Promise<Accountqueryresponse> {
+        try {
+            const destination = this.destinationService.getDestination();
+
+            return await AccountApi.queryaccountserviceAccount()
+                .execute({
+                    destinationName: destination.name
+                })
+                .catch((error) => {
+                    Logger.error(
+                        `SAP Error: ${JSON.stringify(error.response?.data || error.message)}`
+                    );
+                    throw new HttpException(
+                        `Failed to get accounts - ${error.message}`,
+                        500
+                    );
+                });
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            }
+            throw new HttpException(
+                `Failed to get accounts - ${error.message}`,
+                500
+            );
+        }
+    }
+
+    async getAccountById(id: string): Promise<Accountfile> {
+        try {
+            const destination = this.destinationService.getDestination();
+
+            return await AccountApi.readaccountserviceAccount(id)
+                .execute({
+                    destinationName: destination.name
+                })
+                .catch((error) => {
+                    Logger.error(
+                        `SAP Error: ${JSON.stringify(error.response?.data || error.message)}`
+                    );
+                    throw new HttpException(
+                        `Failed to get account by ID - ${error.message}`,
+                        500
+                    );
+                });
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            }
+            throw new HttpException(
+                `Failed to get account by ID - ${error.message}`,
+                500
+            );
+        }
+    }
+}
