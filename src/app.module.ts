def449078968276fcfc4bc0c46ac84bc03ef9518
Raf-VH <raf.vanhoegaerden@outlook.com>
2025-02-13 14:14:37 +0100

    Update app-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DestinationService } from './destination/destination.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [],
+    imports: [ConfigModule.forRoot()],
     controllers: [AppController],
     providers: [AppService, DestinationService]
 })
